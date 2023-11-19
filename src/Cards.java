6e0b2652819c61434833496268af514d5715763b
alisayali <alisayerecebali@gmail.com>
2023-11-19 14:37:12 +0200

    add some luck cards

--- src/Cards.java
+++ src/Cards.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Cards implements Square {
     private int position;
     private String name;
-    private Player player;
+    private Player[] player;
 
     public Cards(int position, String name) {
         this.position = position;
@@ -26,11 +26,11 @@ public class Cards implements Square {
         this.name = name;
     }
 
-    public Player getPlayer() {
+    public Player[] getPlayer() {
         return player;
     }
 
-    public void setPlayer(Player player) {
+    public void setPlayer(Player[] player) {
         this.player = player;
     }
 
@@ -39,20 +39,20 @@ public class Cards implements Square {
     public void firstPlay(Player player) {
         Player[] pl =Main.players();
         Random cardsRandom = new Random();
-        if(this.name.equalsIgnoreCase("Obshtestven trezor")){
-            int obshtestvenTrezorRandom = cardsRandom.nextInt(16)+1;
-            switch (obshtestvenTrezorRandom){
+        if (this.name.equalsIgnoreCase("Obshtestven trezor")) {
+            int obshtestvenTrezorRandom = cardsRandom.nextInt(16) + 1;
+            switch (obshtestvenTrezorRandom) {
                 case 1:
                     System.out.println("life insurance maturity. you receive 100$");
-                    player.setCurrentMoney(player.getCurrentMoney()+100);
+                    player.setCurrentMoney(player.getCurrentMoney() + 100);
                     break;
                 case 2:
                     System.out.println("Pay school fees 50$");
-                    player.setCurrentMoney(player.getCurrentMoney()-50);
+                    player.setCurrentMoney(player.getCurrentMoney() - 50);
                     break;
                 case 3:
                     System.out.println("Overpaid taxes are refunded to you. You get 20$");
-                    player.setCurrentMoney(player.getCurrentMoney()+20);
+                    player.setCurrentMoney(player.getCurrentMoney() + 20);
                     break;
                 case 4:
                     System.out.println("you go straight to jail and when you go through the start you don't get 200$");
@@ -88,16 +88,34 @@ public class Cards implements Square {
                 case 16:
                     break;
             }
-        }else if(this.name.equalsIgnoreCase("Luck")){
-            int luckRandom = cardsRandom.nextInt(16)+1;
-            switch (luckRandom){
+        } else if (this.name.equalsIgnoreCase("Luck")) {
+            int luckRandom = cardsRandom.nextInt(16) + 1;
+            switch (luckRandom) {
                 case 1:
+                    System.out.println("Go to Boqna");
+                    player.setCurrentPosition(39);
                     break;
                 case 2:
+                    System.out.println("You escaped jail without paying.");
+                    player.setCurrentPosition(getPosition() + 1);
                     break;
                 case 3:
+                    System.out.println("Go to ploshtat Makedoia");
+                    if (player.getCurrentPosition() >= 0 && player.getCurrentPosition() < 12) {
+                        player.setCurrentPosition(11);
+                    } else if (player.getCurrentPosition() <= 0 && player.getCurrentPosition() > 12) {
+                        player.setCurrentPosition(11);
+                        player.setCurrentMoney(player.getCurrentMoney() + 200);
+                    } else {
+                        System.out.println("You are at plushtat Makedonia");
+                    }
                     break;
                 case 4:
+//                    Player[] players=Main.players();
+//                    System.out.println("You are chosen to be chairman. You must py every one 50$");
+//                    for (int i = 0; i < players.length; i++) {
+//
+//                    }
                     break;
                 case 5:
                     break;
@@ -124,7 +142,7 @@ public class Cards implements Square {
                 case 16:
                     break;
             }
-        }else {
+        } else {
 
         }
     }
